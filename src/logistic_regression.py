1454a4cf5917b1bb15d476dc3919c2f3d4d7298c
NeilBardhan <indraneil.bardhan@gmail.com>
2020-04-01 20:38:53 -0400

    Changed baseline accuracy

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -23,11 +23,14 @@ def logistic_metrics(test, pred):
 #    false_neg = test[test['difference'] == -1].shape[0]
 #    true_pos = test[test['predictions'] == 1].shape[0] - false_pos
     results = confusion_matrix(test['target'], test['predictions'])
+    accuracy = round(accuracy_score(test['target'], test['predictions']), 4)
+    report = classification_report(test['target'], test['predictions'])
     print('Confusion Matrix :')
     print(results) 
-    print('Accuracy Score :', round(accuracy_score(test['target'], test['predictions']), 4))
+    print('Accuracy Score :', accuracy)
     print('Report :')
-    print(classification_report(test['target'], test['predictions']))
+    print(report)
+    return accuracy, report
 
 def baseline_random(train, test):
     random.seed(1)
@@ -42,7 +45,7 @@ def main():
     df = load_data(data_file)
     train, test = train_test_split(df, 0.25)
     pred = baseline_random(train, test)
-    logistic_metrics(test, pred)
+    baseline_accuracy, baseline_report = logistic_metrics(test, pred)
 
 if __name__ == '__main__':
     main()
